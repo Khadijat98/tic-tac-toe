97f5c15219d54c585af072b3ee71e3b8882a5da1
Khadijat Oyeleye <kaydee98@hotmail.co.uk>
2021-11-16 21:17:04 +0000

    moved game.html content to index.html and deleted game.html, edited styling, started adding js functionality

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 // Query selectors
-
+const playerOneName = document.querySelector("#player-one-name");
+const playerTwoName = document.querySelector("#player-two-name");
+const startGame = document.querySelector(".start__btn");
+const ticTacToeSquares = document.querySelectorAll(".tic-tac-toe__square");
+const ticTacToeInstruction = document.querySelector(".tic-tac-toe__instruction");
 
 
 // Home
@@ -9,6 +13,46 @@
   // fill in empty p tags in scoreboard on game page
   // to drop banners congratulating the players after they win a point and after they win the whole game
 
+
+
+  // global variables for players
+  let playerOne;
+  let playerTwo;
+
+  // function for home page form inputs
+  const handleClick = () => {
+    playerOne = playerOneName.value
+    console.log(playerOne)
+    playerTwo = playerTwoName.value
+    console.log(playerTwo)
+  }
+
+  startGame.addEventListener("click", handleClick);
+
+   ticTacToeSquares.forEach((square) => {
+      square.addEventListener("click", (event) => {
+        ticTacToeInstruction.classList.add("hide")
+          // if (playerOne) {
+          //   event.target.innerHTML = "X"
+          // } else {
+          //   event.target.innerHTML = "O"
+          // }
+
+          playerOne = event.target.innerHTML;
+          playerTwo = event.target.innerHTML;
+
+          if (playerOne) {
+            event.target.innerHTML = "O"
+          } else {
+            event.target.innerHTML = "X"
+          }
+      })
+   })
+  
+  
+ // assign noughts to player one and crosses to player two
+   
+  
 
 // Game
 // need to assign noughts to player one and crosses to player two
